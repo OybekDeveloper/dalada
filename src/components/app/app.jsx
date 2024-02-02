e7c031839675ab90fa2c0bbe8c10e08eb6b9efb6
DeveloperOybek <programmeroybek211@gmail.com>
2024-02-02 16:22:35 +0500

    finish him

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -14,7 +14,7 @@ import Footer from "../footer/footer";
 import "./app.scss";
 import Loader from "../loader/loader";
 const App = () => {
-  const [isLoading , setIsLoading]= useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
   // useEffect(()=>{
   //   setTimeout(() => {
@@ -28,51 +28,53 @@ const App = () => {
         <div className="flex justify-center items-center w-full h-screen">
           <Loader />
         </div>
-      ):(
-      <div id="home" className="app relative">
-        <Navbar />
-        <div className="home max-w-[1440px] xl:w-10/12 max-xl:w-11/12 max-sm:w-full mx-auto flex flex-col items-center mt-[96px] lg:h-[768px] md:h-[680px] sm:h-[600px] max-sm:h-[800px] rounded-[24px] max-sm:rounded-none text-[#fff] overflow-hidden mb-[30px]">
-          <h1 className="text-4xl font-[600] pt-[88px] text-center ">
-            The opportunity to change lives using <br />
-            blockchain technology
-          </h1>
-          <p className="text-base font-[500] pt-[12px] text-center">
-            Security and QoS issues in blockchain enabled next-generation smart
-            logistic <br /> networks: A tutorial
-          </p>
-          <div className="flex flex-row justify-center items-center gap-[12px]">
-            <a href="https://www.apple.com/app-store/">
-              <img width={150} src={appstore} alt="applogo" />
-            </a>
-            <a href="https://play.google.com/store">
-              <img width={150} src={googleplay} alt="applogo" />
-            </a>
+      ) : (
+        <div id="home" className="app relative">
+          <Navbar />
+          <div className="container pt-[83px]">
+            <div className="home max-w-[1440px] xl:w-10/12 max-xl:w-11/12 max-sm:w-full mx-auto flex flex-col items-center  lg:h-[768px] md:h-[680px] sm:h-[600px] max-sm:h-[800px] rounded-[24px] max-sm:rounded-none text-[#fff] overflow-hidden mb-[30px]">
+              <h1 className="text-4xl font-[600] pt-[88px] text-center ">
+                The opportunity to change lives using <br />
+                blockchain technology
+              </h1>
+              <p className="text-base font-[500] pt-[12px] text-center">
+                Security and QoS issues in blockchain enabled next-generation smart
+                logistic <br /> networks: A tutorial
+              </p>
+              <div className="flex flex-row justify-center items-center gap-[12px]">
+                <a href="https://www.apple.com/app-store/">
+                  <img width={150} src={appstore} alt="applogo" />
+                </a>
+                <a href="https://play.google.com/store">
+                  <img width={150} src={googleplay} alt="applogo" />
+                </a>
+              </div>
+              <div className="h-full w-full imgs flex items-start justify-center gap-[32px]">
+                <img
+                  className="xl:w-[320px] lg:w-[270px] md:w-[210px] sm:w-[180px] max-sm:hidden max-md:mt-[100px] mt-[200px]"
+                  src={istaklar1}
+                  alt="appfoto"
+                />
+                <img
+                  className="xl:w-[320px] lg:w-[270px] md:w-[210px] sm:w-[180px]  max-md:mt-[0] mt-[0]"
+                  src={istaklar2}
+                  alt="appfoto"
+                />
+                <img
+                  className="xl:w-[320px] lg:w-[270px] md:w-[210px] sm:w-[180px] max-sm:hidden max-md:mt-[50px] mt-[100px]"
+                  src={istaklar3}
+                  alt="appfoto"
+                />
+              </div>
+            </div>
+            <Features />
+            <About />
+            <Comments />
+            <Blog />
+            <Faq />
           </div>
-          <div className="h-full w-full imgs flex items-start justify-center gap-[32px]">
-            <img
-              className="xl:w-[320px] lg:w-[270px] md:w-[210px] sm:w-[180px] max-sm:hidden max-md:mt-[100px] mt-[200px]"
-              src={istaklar1}
-              alt="appfoto"
-            />
-            <img
-              className="xl:w-[320px] lg:w-[270px] md:w-[210px] sm:w-[180px]  max-md:mt-[0] mt-[0]"
-              src={istaklar2}
-              alt="appfoto"
-            />
-            <img
-              className="xl:w-[320px] lg:w-[270px] md:w-[210px] sm:w-[180px] max-sm:hidden max-md:mt-[50px] mt-[100px]"
-              src={istaklar3}
-              alt="appfoto"
-            />
-          </div>
+          <Footer />
         </div>
-        <Features />
-        <About />
-        <Comments />
-        <Blog />
-        <Faq />
-        <Footer />
-      </div>
       )}
     </>
   );
